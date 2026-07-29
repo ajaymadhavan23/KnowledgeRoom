8589b527ac404302382d0aece0ebcc955004a345
Ajay Madhavan <ajaymadhavan23@gmail.com>
2026-07-29 13:24:00 +0530

    Enforce native ESM for Vercel Serverless Function via api/package.json and root dependencies

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-export default async function handler(req, res) {
-  const { default: app } = await import("../server/src/app.js");
-  return app(req, res);
-}
+import app from "../server/src/app.js";
+
+export default app;
